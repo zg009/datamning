5f53055a9c6713ac16c4409a33d66c62ddcb8001
Zach <zag0003@auburn.edu>
2024-04-25 21:19:31 -0500

    sessions preprocessing decent

--- preprocessing_sessions.py
+++ preprocessing_sessions.py
@@ -55,23 +55,23 @@ def len_of_action_from_series(it, action):
     # print(list(f))
     return len(list(f))
 
-# df_actions.reduce_actions.apply(len_of_action_from_series, args=("show",)).sum()
-action_frames = {}
+action_frames = []
 actions_set = set(df_sessions_no_nulls_action.action.values)
 for action in actions_set:
-    action_frames[action] = df_actions.reduce_actions.apply(len_of_action_from_series, args=(action,)) 
-    # action_frames.append(df_actions.reduce_actions.apply(len_of_action_from_series, args=(action,)))
+    action_frames.append(df_actions.reduce_actions.apply(len_of_action_from_series, args=(action,)).rename(action))
 
 
-action_frames['main'] = df_actions                    
-action_result = pd.concat(action_frames.values(), axis=1)
+action_frames.insert(0, df_actions)                    
+action_result = pd.concat(action_frames, axis=1)
 
 action_type_frames = []
 action_type_set = set(df_sessions_no_nulls_action_type.action_type.values)
 for action_type in action_type_set:
-    action_type_frames.append(df_action_types.reduce_actions.apply(len_of_action_from_series, args=(action_type,)))
+    action_type_frames.append(df_action_types.reduce_actions.apply(len_of_action_from_series, args=(action_type,)).rename(action_type))
 
 
 action_type_frames.insert(0, df_action_types)
 action_type_result = pd.concat(action_type_frames, axis=1)
-# df_sessions_total_seconds.merge(df_unique_devices, left_index=True, right_index=True).merge(action_type_result, left_index=True, right_index=True).columns
+df_all = df_sessions_total_seconds.merge(df_unique_devices, left_index=True, right_index=True).merge(action_type_result, left_index=True, right_index=True).merge(action_result, left_index=True, right_index=True)
+df_all.drop(labels=['reduce_actions_x', 'reduce_actions_y'], axis=1, inplace=True)
+df_all.to_csv('sessions_agg_data.csv')
