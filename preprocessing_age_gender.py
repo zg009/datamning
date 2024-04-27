3feb3deed54ba87b81f00a198b700806c5179679
Zach <zag0003@auburn.edu>
2024-04-27 12:36:49 -0500

    fixed age gender preprocessing

--- preprocessing_age_gender.py
+++ preprocessing_age_gender.py
@@ -12,8 +12,7 @@ df_age_gender_male = df_age_gender.drop(columns=['country_destination', 'age_buc
 df_age_gender_female = df_age_gender.loc[df_age_gender['gender'] == 'female']
 df_age_gender_joined = df_age_gender_female.merge(df_age_gender_male, left_on='join_key', right_on='join_key')
 df_age_gender_joined = df_age_gender_joined.loc[df_age_gender_joined['gender_y'] == 'male']
-# fix this line v
-df_age_gender_joined = df_age_gender_joined.drop(columns=['key_x', 'join_key', 'key_y'])
+df_age_gender_joined = df_age_gender_joined.drop(columns=['year_x', 'join_key', 'year_y'])
 df_age_gender_joined['male_pop_diff'] = df_age_gender_joined['population_in_thousands_y'] - df_age_gender_joined['population_in_thousands_x']
 df_age_gender_joined['more_male'] = np.where(df_age_gender_joined['male_pop_diff'] > 0, 1, 0)
 df_age_gender_joined.to_csv('age_gender_agg_data.csv')
